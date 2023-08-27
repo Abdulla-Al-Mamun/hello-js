70c0f06e149756930667ef5f54d600fb52f7fbfc
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-08-27 19:49:46 +0600

    ok done result calculation

---
+++ resultCalculation.js
@@ -0,0 +1,10 @@
+var math = 75.25;
+var bio = 65;
+var chem = 80;
+var phy = 35.45;
+var bang = 99.50;
+
+var totalNumber = (math + bio + chem + phy + bang);
+
+var result = parseFloat(totalNumber / 5).toFixed(2);
+console.log(result);
