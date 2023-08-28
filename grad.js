27852987ee3afc9cc76f86a6a35288789ac177e7
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-08-28 23:54:06 +0600

    Grade Calculation

---
+++ grad.js
@@ -0,0 +1,40 @@
+// var mamun = 85;
+// var tom = 66;
+// var jane = 95;
+// var peter = 56;
+// var john = 40;
+let number = 50;
+
+
+if (number > 80) {
+    console.log('Grade A');
+}
+else if (number > 60) {
+    console.log('Grade B');
+}
+else if (number > 50) {
+    console.log('Grade C');
+}
+else if (number > 40) {
+    console.log('Grade D');
+}
+else if (number < 39) {
+    console.log('Grade F');
+}
+
+// if (mamun > 80 || tom > 80 || jane > 80 || peter > 80 || john > 80) {
+//     console.log('A Grade')
+// }
+// else if (mamun > 80 || tom > 80 || jane > 80 || peter > 80 || john > 80) {
+//     console.log('B Grade')
+// }
+// else if (mamun > 80 || tom > 80 || jane > 80 || peter > 80 || john > 80) {
+//     console.log('C Grade')
+// }
+// else if (mamun > 80 || tom > 80 || jane > 80 || peter > 80 || john > 80) {
+//     console.log('D Grade')
+// }
+// else {
+//     console.log('F Grade')
+
+// }
