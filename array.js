e14142d2139c3de077eae9eeb9dafe042a23e87f
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-08-28 21:05:13 +0600

    ..

--- array.js
+++ array.js
@@ -1,3 +1,7 @@
 var age = [10, 20, 30, 40, 50];
 // console.log(age.length);
-console.log(age);
+// console.log(age[2])
+age[2] = 100
+// console.log(age[2])
+// console.log(age)
+console.log(age.indexOf(100))
