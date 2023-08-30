a6f5bfa8a12f0f571bbe5be9c761d3a028663ce4
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-08-31 00:09:02 +0600

    2nd problem

--- practice.js
+++ practice.js
@@ -1,5 +1,11 @@
 // ............01...........
 
-for (i = 0; i < 100; i++) {
-    console.log("ajke amar mon valo nei", i)
+// for (i = 0; i < 100; i++) {
+//     console.log("ajke amar mon valo nei", i)
+// }
+
+// ............02...........
+
+for (i = 58; i <= 98; i++) {
+    console.log(i)
 }
