dd75057a2b15ce0954beb1bb29769b580b6025f5
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-08-31 00:43:55 +0600

    problem 05

--- practice.js
+++ practice.js
@@ -18,6 +18,21 @@
 
 // ............04...........
 
-for (i = 581; i <= 623; i += 2) {
-    console.log(i)
+// for (i = 581; i <= 623; i += 2) {
+//     console.log(i)
+// }
+
+// ............05...........
+
+var prices = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 210, 220, 230, 240, 250, 260, 270, 280, 200];
+
+// console.log(price)
+
+for (var i = 0; i < prices.length; i++) {
+    var price = prices[i];
+    if (price > 200) {
+        // break;
+        continue;
+    }
+    console.log(price);
 }
