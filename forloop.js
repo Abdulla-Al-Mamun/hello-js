2dfaf696277f4dd7641acae9a08790942245f6f8
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-08-30 19:47:05 +0600

    for lood v2

--- forloop.js
+++ forloop.js
@@ -1,3 +1,12 @@
-for (var i = 1; i <= 100; i += 2) {
-    console.log(i)
+// for (var i = 1; i <= 100; i += 2) {
+//     console.log(i)
+// }
+
+var friends = ['rony', 'ovi', 'niloy', 'siam', 'mahmud'];
+
+for (var i = 0; i <= friends.length; i++) {
+
+    var friend = friends[i];
+    console.log(friend);
+
 }
