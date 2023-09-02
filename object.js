ac2a60374d4177277d0935ef06d799e5b2d8d934
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-03 00:30:48 +0600

    object line and type add

--- object.js
+++ object.js
@@ -7,4 +7,20 @@ var myComputer = {
 
 console.log(myComputer);
 myComputer.processor = "i21";
+myComputer['price'] = 500;
 console.log(myComputer);
+
+var bands = myComputer.band;
+
+var bands2 = myComputer['band'];
+
+var propertyName = "band"
+var propertValu = myComputer[propertyName];
+
+
+var properties = Object.keys(myComputer);
+
+var properties2 = Object.values(myComputer);
+console.log(properties);
+console.log(properties2);
+
