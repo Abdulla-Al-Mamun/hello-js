4a7d7824b115181b168908bcbf90df8a0e477558
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-03 00:12:48 +0600

    object js

---
+++ object.js
@@ -0,0 +1,10 @@
+var myComputer = {
+    band: "asus",
+    price: 2000,
+    color: "white",
+    processor: "i7",
+}
+
+console.log(myComputer);
+myComputer.processor = "i21";
+console.log(myComputer);
