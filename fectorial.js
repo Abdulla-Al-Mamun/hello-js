2e952d7690a1f6f7a55dc409ee6d0b939af25059
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-05 00:04:49 +0600

    reverse factorial useing while loop

--- fectorial.js
+++ fectorial.js
@@ -1,7 +1,23 @@
+// function factorial(number) {
+//     let result = 1;
+//     for (let i = 1; i <= number; i++) {
+//         result = result * i;
+//     }
+//     return result;
+// }
+
+// const result = factorial(9);
+// console.log(result);
+
+
+// factorial reverse by while loop
+
 function factorial(number) {
+    let i = number;
     let result = 1;
-    for (let i = 1; i <= number; i++) {
+    while (i >= 1) {
         result = result * i;
+        i--;
     }
     return result;
 }
