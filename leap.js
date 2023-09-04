8cadaa2514f976a5ff36287dc44ad93348c183e6
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-05 00:56:10 +0600

    find leap year from array and show this leap year of array

--- leap.js
+++ leap.js
@@ -1,12 +1,33 @@
-function isLeapYear(year) {
-    const remainder = year % 4;
-    if (remainder === 0) {
-        return true;
+// function isLeapYear(year) {
+//     const remainder = year % 4;
+//     if (remainder === 0) {
+//         return true;
+//     }
+//     return false;
+// }
+
+// const 
+// const leapYear = isLeapYear(2023);
+
+// console.log(leapYear);
+
+// fine leap years and add a array 
+
+
+function findLeapYear(year) {
+    // console.log(years);
+    const newLeapYear = [];
+    for (let i = 0; i < year.length; i++) {
+        const index = i;
+        const element = year[index];
+        // console.log(index, element);
+        if (element % 2 === 0) {
+            newLeapYear.push(element);
+        }
     }
-    return false;
+    return newLeapYear;
 }
 
-
-const leapYear = isLeapYear(2023);
-
+const years = [2023, 2024, 2025, 2028, 2030];
+const leapYear = findLeapYear(years);
 console.log(leapYear);
