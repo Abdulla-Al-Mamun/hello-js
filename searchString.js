35244466773321e67347246a73636db5e29fbabb
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-06 00:44:22 +0600

    search string and chack include exists value inside the string

---
+++ searchString.js
@@ -0,0 +1,12 @@
+const name = 'abdulla al mamun';
+const searchString = 'Al';
+const doesExist = name.toLowerCase().includes(searchString.toLowerCase());
+console.log(doesExist);
+
+if (name.indexOf('alll') !== -1) {
+    console.log('exists inside the string');
+}
+else {
+    console.log('exists not inside the string');
+
+}
