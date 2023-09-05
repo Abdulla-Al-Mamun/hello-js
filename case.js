6400a547dee5a03e9701f7617a2d226f70a719f8
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-06 00:11:44 +0600

    chaking string value is a valid or not valid

---
+++ case.js
@@ -0,0 +1,9 @@
+const userName = 'abdullaH';
+const userInput = 'abdullah';
+
+if (userInput.toLocaleLowerCase === userName.toLocaleLowerCase) {
+    console.log('your value is valid');
+}
+else {
+    console.log('your value is not valid');
+}
