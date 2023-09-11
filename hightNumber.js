cbcf48a4309adbf3948029bd3bf45ab294500e49
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2023-09-11 22:47:02 +0600

    lergest heights function

---
+++ hightNumber.js
@@ -0,0 +1,17 @@
+function hightNumberInArray(numbers) {
+    let largest = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+
+        const index = i;
+        const element = numbers[index];
+        // console.log(element);
+        if (element > largest) {
+            largest = element;
+        }
+    }
+    return largest;
+}
+
+const heights = [155, 160, 125, 185, 256];
+const tallest = hightNumberInArray(heights);
+console.log(tallest);
